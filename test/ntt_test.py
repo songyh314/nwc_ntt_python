e28458da144399c46aae02ea8f5da4f4666962a5
songyh <song_yh314@163.com>
2024-09-15 18:27:14 +0800

    Add Conv Test

--- test/ntt_test.py
+++ test/ntt_test.py
@@ -5,7 +5,8 @@ import ntt_fun
 import tool
 import math
 import param
-from param import MOD,HalfMod,PolyMax
+from param import MOD, HalfMod, PolyMax
+
 
 @pytest.mark.NttInttTest
 def test_ntt_intt():
@@ -21,4 +22,29 @@ def test_ntt_intt():
         a[i] = -1
     nttRes = applyNttSignedInput(a, nwc_param.w_rom, N)
     inttRes = applyInttSignedOutput(nttRes, nwc_param.inv_w_rom, N)
+    assert a == inttRes
+    print()
     print(inttRes)
+
+
+def test_conv():
+    N = 512
+    lvl = int(math.log2(N))
+    tw_rom = nwc_tw.TW_ROM(N)
+    nwc_param = nwc_tw.NWC_PARAM(lvl)
+    nwc_tw.gen_tw(tw_rom)
+    nwc_tw.genNWC(tw_rom, nwc_param, "NWC-DIF-RN-INNT")
+    nwc_tw.genNWC(tw_rom, nwc_param, "NWC-DIT-NR-NNT")
+    a = list(0 for _ in range(N))
+    b = list(0 for _ in range(N))
+    for i in range(N):
+        a[i] = 1
+        b[i] = 1
+    NttRes1 = applyNttSignedInput(a, nwc_param.w_rom, N)
+    NttRes2 = applyNttSignedInput(b, nwc_param.w_rom, N)
+    mulRes = list(0 for _ in range(N))
+    for i in range(N):
+        mulRes[i] = (NttRes1[i] * NttRes2[i]) % MOD
+    InttRes = applyInttSignedOutput(mulRes,nwc_param.inv_w_rom,N)
+    print()
+    print((InttRes))
